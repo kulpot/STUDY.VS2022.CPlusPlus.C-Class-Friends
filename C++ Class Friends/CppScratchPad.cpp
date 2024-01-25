765899e72b33631acb813e76c05dc95d1ef1871a
Kulpot <kulpotwahu@gmail.com>
2024-01-25 16:35:04 +0800

    ...

--- C++ Class Friends/CppScratchPad.cpp
+++ C++ Class Friends/CppScratchPad.cpp
@@ -2,8 +2,8 @@
 using std::cout;	// cout: is an output stream, sending data out the console
 using std::endl;
 
-// -------------------- C++OverloadngLeftShiftOperatrInPlaceofWritngToString() ---------------------------------
-//ref link:https://www.youtube.com/watch?v=1coNjTrOs7w&list=PLRwVmtr-pp077ESC0tAC331LxLaDa5A57&index=3
+// ------------------- C++ Class Friends
+//ref link:https://www.youtube.com/watch?v=7N4qcBcLTKU&list=PLRwVmtr-pp077ESC0tAC331LxLaDa5A57&index=4
 
 // << is called stream insertion operator
 // cout: is an output stream, sending data out the console
@@ -24,7 +24,7 @@ Vector operator+(const Vector& left, const Vector& right)
 }
 
 //void operator<<(std::ostream& stream, const Vector& right)		//Parameters - 1st argument: cout ostream ref, 2nd argument: const ref Vector <-- vector modify not use
-std::ostream& operator<<(std::ostream& stream, const Vector& right)		
+std::ostream& operator<<(std::ostream& stream, const Vector& right)
 {
 	cout << "{" << right.x << ", " << right.y << "}";	// error notice:must copy data for each vector into the stream object like so? -> stream << "{" << right.X << ", " << etc.....  its not modifying the stream object created at all but are just calling 'cout' from within the method.
 	return stream;	// void must be std::ostream&
@@ -37,6 +37,75 @@ void main()
 	//cout << result << endl;		//error:needs return stream // "<<"(stream insertion operator) needs overloading //cout << endl;	- cout return result in its place
 	cout << first << " + " << second << " = " << result << endl;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------- C++OverloadngLeftShiftOperatrInPlaceofWritngToString() ---------------------------------
+//ref link:https://www.youtube.com/watch?v=1coNjTrOs7w&list=PLRwVmtr-pp077ESC0tAC331LxLaDa5A57&index=3
+
+// << is called stream insertion operator
+// cout: is an output stream, sending data out the console
+
+//struct Vector		// algebra/trigonometry knowledge required
+//{
+//	int x;			// Notice:float is default type for vector
+//	int y;
+//	Vector(int x = 0, int y = 0) : x(x), y(y) {}
+//};
+//
+//Vector operator+(const Vector& left, const Vector& right)
+//{
+//	Vector ret;
+//	ret.x = left.x + right.x;
+//	ret.y = left.y + right.y;
+//	return ret;
+//}
+//
+////void operator<<(std::ostream& stream, const Vector& right)		//Parameters - 1st argument: cout ostream ref, 2nd argument: const ref Vector <-- vector modify not use
+//std::ostream& operator<<(std::ostream& stream, const Vector& right)		
+//{
+//	cout << "{" << right.x << ", " << right.y << "}";	// error notice:must copy data for each vector into the stream object like so? -> stream << "{" << right.X << ", " << etc.....  its not modifying the stream object created at all but are just calling 'cout' from within the method.
+//	return stream;	// void must be std::ostream&
+//}
+//
+//void main()
+//{
+//	Vector first(1, 2), second(3, 4);
+//	Vector result = first + second;		// + is operator+
+//	//cout << result << endl;		//error:needs return stream // "<<"(stream insertion operator) needs overloading //cout << endl;	- cout return result in its place
+//	cout << first << " + " << second << " = " << result << endl;
+//}
 
 
 
